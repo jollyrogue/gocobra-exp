e40639d7ff8c4593ff3c7b10a1a01541933182a4
Ryan Quinn <quinn.rm@gmail.com>
2019-01-03 22:52:39 +0000

    Parsing the config file and printing found values.
     Changes to be committed:
    	modified:   cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -18,12 +18,29 @@ import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+type ConfigTreeMain struct {
+	Servers []string
+	Secure  bool
+	Domain  string
+}
+
+type ConfigTreeDoSomething struct {
+	Repeat   int
+	Sentance string
+}
+
+type ConfigTree struct {
+	Number      int
+	Main        ConfigTreeMain
+	DoSomething ConfigTreeDoSomething
+}
+
 var cfgFile string
+var cfgTree ConfigTree
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,7 +54,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	//Run: func(cmd *cobra.Command, args []string) {},
+	Run: mainConfigPrint,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -55,11 +73,11 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gocobra-exp.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.gocobra-exp.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -68,16 +86,8 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".gocobra-exp" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".gocobra-exp")
+		viper.AddConfigPath(".")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
@@ -85,5 +95,25 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		err := viper.Unmarshal(&cfgTree)
+		if err != nil {
+			fmt.Printf("Unable to decode config into struct, %v\n", err)
+		}
 	}
+
+}
+
+func mainConfigPrint(cmd *cobra.Command, args []string) {
+	fmt.Println("Printing config tree.")
+	fmt.Printf("Full Config Tree:\n%+v\n", cfgTree)
+	fmt.Println("")
+	fmt.Printf("root.Number: %d\n", cfgTree.Number)
+	fmt.Printf("root.Main.Servers: %s\n", cfgTree.Main.Servers)
+	for i, v := range cfgTree.Main.Servers {
+		fmt.Printf("\t%d: %s\n", i, v)
+	}
+	fmt.Printf("root.Main.Secure: %t\n", cfgTree.Main.Secure)
+	fmt.Printf("root.Main.Domain: %s\n", cfgTree.Main.Domain)
+	fmt.Printf("root.DoSomething.Repeat: %d\n", cfgTree.DoSomething.Repeat)
+	fmt.Printf("root.DoSomething.Sentance: %s\n", cfgTree.DoSomething.Sentance)
 }
