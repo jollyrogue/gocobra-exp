9434e04ac87791dbc18656f2b076317489d9a48b
Ryan Quinn <quinn.rm@gmail.com>
2019-01-04 17:29:04 +0000

    Updated help description and added doSomethingMain function to print out the parsed config tree.
     Changes to be committed:
    	modified:   cmd/dosomething.go

--- cmd/dosomething.go
+++ cmd/dosomething.go
@@ -23,16 +23,12 @@ import (
 // dosomethingCmd represents the dosomething command
 var dosomethingCmd = &cobra.Command{
 	Use:   "dosomething",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dosomething called")
-	},
+	Short: "It does something, anything really.",
+	Long:  `dosomething subcommand prints the sentence `,
+	//Run: func(cmd *cobra.Command, args []string) {
+	//	fmt.Println("dosomething called")
+	//},
+	Run: doSomethingMain,
 }
 
 func init() {
@@ -47,4 +43,9 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// dosomethingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func doSomethingMain(cmd *cobra.Command, args []string) {
+	fmt.Println("dosomething called")
+	fmt.Printf("Config Tree: %+v\n", cfgTree)
 }
